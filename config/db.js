a5dbd5c91d1df252d1362468a219aeb0df9a1712
ArjunBhandari2511 <mailarjunbhandari7@gmail.com>
2025-02-01 20:24:20 +0530

    MongoDB vercel.json

--- config/db.js
+++ config/db.js
@@ -1,19 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const uri = process.env.MONGO_URI;
+let isConnected = false; // Global variable
 
-async function connectDB() {
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Connected to MongoDB Atlas!");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-    }
-}
+const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+  }
+};
 
 module.exports = connectDB;
