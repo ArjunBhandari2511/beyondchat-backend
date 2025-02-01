692938c8bde3c7e221d0a2e513b023b7b35fbb70
ArjunBhandari2511 <mailarjunbhandari7@gmail.com>
2025-02-01 21:03:23 +0530

    Cors Change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow frontend URL
+    origin: "https://beyondchat-frontend.vercel.app", // Allow frontend URL
     credentials: true, // Allow cookies & sessions
   })
 );
